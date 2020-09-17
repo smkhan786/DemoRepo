5d0d85b2a6b329f326faf7a9a9b9f8f885153f48
Khan <sahil-mohammed.khan@capgemini.com>
2020-09-17 15:00:41 -0400

    first git commit for my selenium code

---
+++ src/test/java/com/qa/tests/DemoTest.java
@@ -0,0 +1,49 @@
+package com.qa.tests;
+
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+	@Test
+	public void sum() {
+		System.out.println("Sum");
+		
+		int a = 10;
+		int b = 20;
+		
+		Assert.assertEquals(30, a+b);
+	}
+
+	@Test
+	public void sub() {
+		System.out.println("Sub");
+		
+		int a = 10;
+		int b = 20;
+		
+		Assert.assertEquals(10, b-a);
+	}
+	@Test
+	public void div() {
+		System.out.println("Div");
+		
+		int a = 10;
+		int b = 20;
+		
+		Assert.assertEquals(2, b/a);
+	}
+	@Test
+	public void multi() {
+		System.out.println("Multiplication");
+		
+		int a = 10;
+		int b = 20;
+		
+		Assert.assertEquals(200, a*b);
+	}
+
+
+ 	
+	
+}
